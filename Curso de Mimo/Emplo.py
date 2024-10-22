a63f42bf7301a8bd22fe65c9b44bd16a7d0b37f2
jesus <yisusfake19@gmail.com>
2024-10-22 17:23:10 -0400

    Estoy de regreso

--- Curso de Mimo/Emplo.py
+++ Curso de Mimo/Emplo.py
@@ -1,8 +1,4 @@
-import string
-import random
-
-def Claves (tamanio = 10, caracter = string.ascii_letters + string.digits):
-    return ''.join (random.choice (caracter) for _ in range (tamanio))
-
-for _ in range (5):
-    print (Claves(6))
+hora = float (input("Horas Trabajadas:" ))
+paga = float (input("horas Pagadas:" ))
+coste = hora * paga
+print ("Tu paga es", coste)
