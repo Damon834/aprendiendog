e0255379fd0f430f393233aa534650af4dd9c8b9
jesus <yisusfake19@gmail.com>
2024-10-22 16:28:00 -0400

    cambio

--- Proyectos Serios/03-Clave_Bucles.py
+++ Proyectos Serios/03-Clave_Bucles.py
@@ -1,7 +1,7 @@
 import string
 import random
 
-def cts ( tamaño = 10, caracteres = string.ascii_letters ):
+def cts ( tamaño = 18, caracteres = string.ascii_letters ):
     return ''.join (random.choice (caracteres) for _ in range (tamaño) ) 
 
 for i in range (4):
